b7c771cf0e83eefeaad5e005259ee3d8c6e28a56
pilpod <67429595+pilpod@users.noreply.github.com>
2020-12-09 22:31:08 +0100

    Inicialize kata

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -4,42 +4,21 @@ namespace App;
 
 class BowlingGame {
 
-    private int $pinsPerBall;
-    private int $scoreResult;
-    private int $scoreCurrent;
-    private int $ballCount;
+    
 
     public function __construct()
     {
-        $this->pinsPerBall = 0;
-        $this->scoreResult = 0;
-        $this->scoreCurrent = 0;
-        $this->ballCount = 0; 
+        
     }
 
     public function roll(int $pinsDown): void
     {
-        $this->ballCount++;
-        $this->pinsPerBall += $pinsDown;
         
-        if($this->ballCount == 2 && $this->pinsPerBall < 10) {
-            $this->scoreCurrent = $this->pinsPerBall;
-            $this->ballCount = 0;
-        }
-        
-        if($this->pinsPerBall == 10 && $this->ballCount == 2) {
-            $this->ballCount = 0;
-        }
-
-        if($this->pinsPerBall > 10 && $this->ballCount == 1) {
-            $this->scoreCurrent += $this->pinsPerBall;
-        }
     }
 
     public function score(): int
     {
-        $this->scoreResult = $this->scoreCurrent;
-        return $this->scoreResult;
+        return 0;
     }
 
 } 
--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -9,27 +9,12 @@ class BowlingGameTest extends TestCase {
 
 	public function test_player_knocked_down_2_and_3_pins_with_2_ball()
 	{
-		$game = new BowlingGame();
-		$game->roll(2);
-		$game->roll(3);
-		$result = $game->score();
+		
 
-		$this->assertEquals(5, $result);
+		$this->assertEquals(5, 5);
 	}
 
-	public function test_player_make_a_spare()
-	{
-		//given
-		$game = new BowlingGame;
-		//when
-		$game->roll(4);
-		$game->roll(6);
-		$game->roll(1);
-		$game->roll(4);
-		$result = $game->score();
-		//then
-		$this->assertEquals(11, $result);
-	}
+	
 
 }
 
