0c1a8304b5907c550b6fcaf883c50bc4f4ec73a9
pilpod <67429595+pilpod@users.noreply.github.com>
2021-01-30 17:39:05 +0100

    test players do ones in all rolls

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -5,10 +5,13 @@ namespace App;
 class BowlingGame {
 
     private int $score = 0;
+    private int $frame = 0;
+    private int $ball = 1;
 
     public function Roll(int $pins)
     {
         $this->score += $pins;
+
     }
 
     public function Score() : int
--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -5,6 +5,8 @@ namespace Tests;
 use PHPUnit\Framework\TestCase;
 use App\BowlingGame;
 
+use function PHPUnit\Framework\assertEquals;
+
 class BowlingGameTest extends TestCase {
 
 	public function test_player_finished_the_game_with_zero_point()
@@ -35,6 +37,23 @@ class BowlingGameTest extends TestCase {
 		$this->assertEquals($expected, $score);
 	}
 	
+	public function test_player_do_one_spare()
+	{
+		$game = new BowlingGame();
+		$expected = 12;
+
+		$game->Roll(4);
+		$game->Roll(6);
+		$game->Roll(1);
+
+		for ($i=0; $i < 18; $i++) { 
+			$game->Roll(0);
+		}
+
+		$score = $game->Score();
+
+		$this->assertEquals($expected, $score);
+	}
 
 	
 
